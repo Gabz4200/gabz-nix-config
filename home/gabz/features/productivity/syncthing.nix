e70599fa754479ca93b72443284b81bc5b6745f4
Joao Gabriel Souza Amaral <jgsa75005@gmail.com>
2025-10-02 18:23:23 -0300

    Added Syncthing

--- home/gabz/features/productivity/syncthing.nix
+++ home/gabz/features/productivity/syncthing.nix
@@ -1,5 +1,35 @@
 {
+  config,
+  lib,
+  ...
+}: let
+  remoteId = "EIDJBPY-QXNRYHF-HC6Q4ER-O5FVG5A-2BZNK4F-Q6DDQC6-744RYJU-4DHZTQR";
+  syncDir = "${config.home.homeDirectory}/Sync";
+in {
   services.syncthing = {
     enable = true;
+    overrideDevices = true;
+    overrideFolders = true;
+    settings = {
+      devices = {
+        "celular" = {
+          id = remoteId;
+          name = "celular";
+          addresses = ["dynamic"];
+          autoAcceptFolders = true;
+        };
+      };
+      folders = {
+        default = {
+          id = "default";
+          label = "Sync";
+          path = syncDir;
+          type = "sendreceive";
+          devices = ["celular"];
+        };
+      };
+    };
   };
+
+  home.persistence."/persist".directories = lib.mkAfter ["Sync"];
 }
