b5428f0db41f4878fc9ae3f1b51a6804c8313fe2
João Gabriel Souza Amaral <39097199+Gabz4200@users.noreply.github.com>
2025-10-03 19:13:08 -0300

    Atualizar o nixpkgs.nix

--- home/gabz/nixpkgs.nix
+++ home/gabz/nixpkgs.nix
@@ -10,16 +10,16 @@
 in (
   {
     nix = {
-      settings = {
-        extra-substituters = lib.mkAfter ["https://cache.m7.rs"];
-        extra-trusted-public-keys = ["cache.m7.rs:kszZ/NSwE/TjhOcPPQ16IuUiuRSisdiIwhKZCxguaWg="];
+     settings = {
+    #   extra-substituters = lib.mkAfter #["https://cache.m7.rs"];
+#        extra-trusted-public-keys = #["cache.m7.rs:kszZ/NSwE/#TjhOcPPQ16IuUiuRSisdiIwhKZCxguaWg="];
         experimental-features = [
           "nix-command"
           "flakes"
           "ca-derivations"
         ];
         warn-dirty = false;
-        flake-registry = ""; # Disable global flake registry
+        #flake-registry = ""; # Disable # global flake registry
       };
       registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
     };
