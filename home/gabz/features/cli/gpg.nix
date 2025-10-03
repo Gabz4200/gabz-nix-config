edaf874b4c70c4e48b3c25ddcd9311629d3c9905
João Gabriel Souza Amaral <39097199+Gabz4200@users.noreply.github.com>
2025-10-03 19:18:06 -0300

    Atualizar o gpg.nix

--- home/gabz/features/cli/gpg.nix
+++ home/gabz/features/cli/gpg.nix
@@ -7,7 +7,7 @@
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
-    sshKeys = ["149F16412997785363112F3DBD713BC91D51B831"];
+    #sshKeys = #["149F16412997785363112F3DBD713BC91D51B831"]#;
     enableExtraSocket = true;
     pinentry.package =
       if config.gtk.enable
